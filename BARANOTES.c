2802fd087a3985fd6526f565f4fd88c349e22f2b
Azrael <61298056+LunaTempest@users.noreply.github.com>
2023-09-22 11:14:58 -0500

    struct made

--- BARANOTES.c
+++ BARANOTES.c
@@ -2,4 +2,21 @@
 #include <stdlib.h> 
 #include <math.h>
 #include <Windows.h>
+#include <ResourceIndexer.h>
+#include <string.h>
 
+#define MAX_SIZE 10000000
+
+struct Database
+{
+    char arr[MAX_SIZE];
+    
+};
+
+void MainMenu();
+void Typer();
+
+int main()
+{
+
+}
